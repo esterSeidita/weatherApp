07dd60505263e3bbf705d226bf5998d6cce684e7
Ester Seidita <101104738+esterSeidita@users.noreply.github.com>
2022-05-26 19:24:43 +0200

    fix: update key and cities

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import './App.css';
-import {CitySelect} from './components/CitySelect';
-import {CityWeather} from './components/CityWeather';
-import {Header} from './components/Header';
-import {useState} from "react";
-
-const apiKey = "1d00d4c9d5554bbb1fc814776caff71d";
-const lang = "it";
+import "./App.css";
+import { CitySelect } from "./components/CitySelect";
+import { CityWeather } from "./components/CityWeather";
+import { Header } from "./components/Header";
+import { useState } from "react";
 
 function App() {
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const lang = "it";
 
   const [city, setCity] = useState("Benvenuto!");
   const [temp, setTemp] = useState("--");
   const [weather, setWeather] = useState("Seleziona una città");
-  const [img, setImg] = useState("https://openweathermap.org/img/wn/11d@2x.png");
+  const [img, setImg] = useState(
+    "https://openweathermap.org/img/wn/11d@2x.png"
+  );
 
-  const update = async(value = "Palermo") => {
+  const update = async (value = "Palermo") => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${value}&units=metric&appid=${apiKey}&lang=${lang}`;
     const response = await fetch(url);
     const data = await response.json();
-    
-    setImg(`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+
+    setImg(`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
     setCity(data.name);
     setWeather(data.weather[0].description);
     setTemp(data.main.temp);
-  }
+  };
 
   return (
     <div className="App">
-      <Header/>
-      <div className='cardWrapper'>
-        <CitySelect  notify={update} />
-        <CityWeather img={img} city={city} weather={weather} temp={temp}/>
+      <Header />
+      <div className="cardWrapper">
+        <CitySelect notify={update} />
+        <CityWeather img={img} city={city} weather={weather} temp={temp} />
       </div>
-      <p><a className="attribute" href="https://www.freepik.com/vectors/cloudy-day">Cloudy day vector created by macrovector - www.freepik.com</a></p>
+      <p className="attribute">
+          Credits of Ester Seidita <br/>for Edgemony Coding Bootcamp
+      </p>
     </div>
   );
 }
